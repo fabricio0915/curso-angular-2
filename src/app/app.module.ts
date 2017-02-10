e3c56247f43388f51f497a4b17a9fc93b2f1291e
fabricio0915 <fabricio.nascimento15@outlook.com>
2017-02-09 22:06:16 -0200

    Roteamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { HeadbarComponent } from './components/shared/headbar/headbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -20,7 +21,8 @@ import { HomePage } from './pages/home-page/home-page';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [HomePage]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,16 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { CourseDetailsComponent } from './components/course-details/course-details.component';
+import { ListCoursesComponent } from './components/list-courses/list-courses.component';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'courses', component: ListCoursesComponent },
+    { path: 'courses/details/:tag', component: CourseDetailsComponent },
+];
+
+export const appRoutingProviders: any[] = [];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
